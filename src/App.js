39442fd79f6e43e079dafeaebec9d0de66415def
Protech317 <shamurzaevbk@gmail.com>
2023-01-14 17:57:40 -0800

    footer done, needs more styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './UI/Card';
+import FullPage from './fullPage/FullPage';
 
 function App() {
   return (
     <div className="App">
+      <FullPage />
     
 
     </div>
